7e6980da5d3ff16579ab5de9596dedf645ba0618
Igor R. <88985765+rpigor@users.noreply.github.com>
2023-12-04 12:18:28 +0100

    Added UniqueTable iterators test

--- src/test/UniqueTableTests.h
+++ src/test/UniqueTableTests.h
@@ -3,7 +3,8 @@
 
 #include <gtest/gtest.h>
 #include "../UniqueTable.h"
-#include <iostream>
+#include <vector>
+#include <string>
 
 class UniqueTableTest : public testing::Test {
 protected:
@@ -33,6 +34,31 @@ TEST_F(UniqueTableTest, SizeWorks)
     ClassProject::Node nodeTrue{1, 1, 1, 1, "True"};
     uniqueTable.insert(nodeTrue);
     EXPECT_EQ(uniqueTable.size(), 2);
+}
+
+TEST_F(UniqueTableTest, IteratorsWork)
+{
+    ClassProject::Node nodeFalse{0, 0, 0, 0, "False"};
+    ClassProject::Node nodeTrue{1, 1, 1, 1, "True"};
+    ClassProject::Node aNode{2, 2, 1, 0, "a"};
+    uniqueTable.insert(nodeFalse);
+    uniqueTable.insert(nodeTrue);
+    uniqueTable.insert(aNode);
+
+    std::vector<std::string> labels = {"False", "True", "a"};
+    unsigned int i = 0;
+    for (auto it = uniqueTable.begin(); it != uniqueTable.end(); it++)
+    {
+        EXPECT_EQ(it->label, labels[i]);
+        i++;
+    }
+
+    i = 0;
+    for (auto it = uniqueTable.cbegin(); it != uniqueTable.cend(); it++)
+    {
+        EXPECT_EQ(it->label, labels[i]);
+        i++;
+    }
 }
 
 #endif
