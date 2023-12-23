06df157cefd9a13bb82848525443970881336346
Igor R. <88985765+rpigor@users.noreply.github.com>
2023-12-23 18:44:51 +0100

    Included benchmark requirements in verification script

--- src/verify/verify.sh
+++ src/verify/verify.sh
@@ -1,11 +1,35 @@
 #!/usr/bin/env bash
 
-circuit=${1:-c3540}
-timeout_time=${2:-20s}
+circuit="c3540"
+runtime_threshold=10.0
+vm_threshold=1000000
+timeout_time="20s"
+
+print_usage () {
+    echo "Usage: $0 [-c benchmark_name] [-r runtime_threshold] [-m memory_threshold] [-t timeout_time]"
+    echo "Example: $0 -c c432 -r 1.0 -m 50000 -t 10s"
+}
+
+while getopts "hc:r:m:t:" option; do
+    case $option in
+        c) circuit="$OPTARG";;
+        r) runtime_threshold="$OPTARG";;
+        m) vm_threshold="$OPTARG";;
+        t) timeout_time="$OPTARG";;
+        h)
+            print_usage
+            exit 0
+            ;;
+        *)
+            print_usage
+            exit 1
+            ;;
+    esac
+done
 
 cd "$(dirname "${BASH_SOURCE[0]}")"
 if ! [[ -d ../../build ]]; then
-    >&2 echo "[ERROR] Binaries were not built."
+    >&2 echo "[ERROR] Could not find build directory."
     exit 1
 fi
 cd "../../build"
@@ -14,12 +38,49 @@ if ! [[ -f "../benchmarks/iscas85/$circuit.bench" ]]; then
     exit 1
 fi
 
-echo "[INFO] Running ISCAS'85 $circuit benchmark."
-./src/bench/VDSProject_bench "../benchmarks/iscas85/$circuit.bench"
+echo "[INFO] Running ISCAS'85 $circuit benchmark..."
+time_start=$(date +%s%3N)
+bench_result=$(./src/bench/VDSProject_bench "../benchmarks/iscas85/$circuit.bench")
+time_end=$(date +%s%3N)
+time_ms=$(( $time_end - $time_start ))
 if ! [[ -d "results_$circuit/txt" ]]; then
     >&2 echo "[ERROR] Could not find reference results."
     exit 1
 fi
+
+within_requirements=true
+runtime_regex="Runtime: ([0-9]+.[0-9]+)"
+if [[ $bench_result =~ $runtime_regex ]]; then
+    runtime="${BASH_REMATCH[1]}"
+    echo "[!] Runtime: $runtime"
+else
+    >&2 echo "[ERROR] Could not match benchmark runtime."
+    exit 1
+fi
+vm_regex="VM: ([0-9]*);"
+if [[ $bench_result =~ $vm_regex ]]; then
+    vm="${BASH_REMATCH[1]}"
+    echo "[!] VM usage: $vm"
+else
+    >&2 echo "[ERROR] Could not match benchmark VM usage."
+    exit 1
+fi
+
+if [ "`echo "${runtime} < ${runtime_threshold}" | bc`" -ne 1 ]; then
+    >&2 echo "[ERROR] Benchmark is not within runtime requirements ($runtime >= $runtime_threshold)."
+    within_requirements=false
+fi
+if [ "`echo "${vm} < ${vm_threshold}" | bc`" -ne 1 ]; then
+    >&2 echo "[ERROR] Benchmark is not within VM usage requirements ($vm >= $vm_threshold)."
+    within_requirements=false
+fi
+if [ "$within_requirements" = false ]; then
+    exit 1
+else
+    echo "[SUCCESS] Runtime and VM usage are within the requirements."
+fi
+
+echo "[INFO] Finished executing benchmark! ($time_ms ms)"
 
 i=1
 files=(results_$circuit/txt/*)
